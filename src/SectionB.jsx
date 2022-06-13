d39cdca85a4eda4a8d9601f9c0471f39461cd6c7
Al3x3ySh3vch3nko <a.r.shevchenko@gmail.com>
2022-06-13 18:37:18 +0300

    +

--- src/SectionB.jsx
+++ src/SectionB.jsx
@@ -1,9 +1,134 @@
 /* eslint-disable import/no-anonymous-default-export */
+// import { render } from '@testing-library/react'
 import React from 'react'
 import SkillLine from './SkillLine'
 
+const attributes_SkillLine =
+[
+    {
+        id    : 1,
+        name  : 'HTML & CSS',
+        width : '90%'
+    },
+    {
+        id    : 2,
+        name  : 'Design',
+        width : '60%'
+    },
+    {
+        id    : 3,
+        name  : 'Typography',
+        width : '70%'
+    },
+    {
+        id    : 4,
+        name  : 'Zepplin',
+        width : '100%'
+    },
+    {
+        id    : 5,
+        name  : 'Adobe Photoshop',
+        width : '50%'
+    },
+    {
+        id    : 6,
+        name  : 'Figma',
+        width : '60%'
+    },
+    {
+        id    : 7,
+        name  : 'SASS',
+        width : '60%'
+    },
+    {
+        id    : 8,
+        name  : 'Bootstrap',
+        width : '70%'
+    },
+    {
+        id    : 9,
+        name  : 'GIT, GITHUB',
+        width : '70%'
+    },
+    {
+        id    : 10,
+        name  : 'Gulp',
+        width : '30%'
+    },
+    { 
+        id    : 11,
+        name  : 'Java Script',
+        width : '60%'
+    },
+    {
+        id    : 12,
+        name  : 'React',
+        width : '45%'
+    },
+    {
+        id    : 13,
+        name  : 'Node JS \ Express JS',
+        width : '30%'
+    },
+    {
+        id    : 14,
+        name  : 'API (REST)',
+        width : '40%'
+    },
+    {
+        id    : 15,
+        name  : 'EJS',
+        width : '60%'
+    },
+    {
+        id    : 16,
+        name  : 'SQL',
+        width : '30%'
+    },
+    {
+        id    : 17,
+        name  : 'MongoDB \ Mongoose',
+        width : '30%'
+    },
+    {
+        id    : 18,
+        name  : 'Authentication & Security',
+        width : '30%'
+    },
+    {
+        id    : 19,
+        name  : 'Wordpress',
+        width : '30%'
+    },
+    {
+        id    : 20,
+        name  : 'JQuery',
+        width : '70%'
+    },
+    {
+        id    : 21,
+        name  : 'PHP',
+        width : '10%'
+    },
+    {
+        id    : 22,
+        name  : 'WEB 3 (Motoko, Decentrelized Apps (DApps), Internet Computer, Blockchain)',
+        width : '15%'
+    },
+]
 
-export default () =>
+const create_SkillLine = (attribute) =>
+{
+    return(
+    <SkillLine
+        name  = {attribute.name}
+        width = {attribute.width}
+        key   = {attribute.id}
+    />  
+    )
+}
+
+const SectionB = () =>
 (
 <section id="about-b" className = "py-1">
     <div className = "container frost">
@@ -13,98 +138,7 @@ export default () =>
         <span className = "red">]</span>
     </h2>
     <div className = "bottom-line"></div>
-    <SkillLine
-        name = 'HTML & CSS'
-        width = '90%'
-    />        
-    <SkillLine
-        name  = 'Design'
-        width = '60%'
-    />  
-    <SkillLine
-        name  = 'Typography'
-        width = '70%'
-    />  
-    <SkillLine
-        name  = 'Zepplin'
-        width = '100%'
-    />  
-    <SkillLine
-        name  = 'Adobe Photoshop'
-        width = '50%'
-    />  
-    <SkillLine
-        name  = 'Figma'
-        width = '60%'
-    />  
-    <SkillLine
-        name  = 'SASS'
-        width = '60%'
-    />  
-    <SkillLine
-        name  = 'Bootstrap'
-        width = '70%'
-    />  
-    <SkillLine
-        name  = 'GIT, GITHUB'
-        width = '70%'
-    />  
-    <SkillLine
-        name  = 'Gulp'
-        width = '30%'
-    />  
-    <SkillLine
-        name  = 'Java Script'
-        width = '60%'
-    />  
-    <SkillLine
-        name  = 'React'
-        width = '45%'
-    />  
-    <SkillLine
-        name  = 'Node JS \ Express JS'
-        width = '30%'
-    />  
-    <SkillLine
-        name  = 'API (REST)'
-        width = '40%'
-    />  
-    <SkillLine
-        name  = 'EJS'
-        width = '60%'
-    />  
-    <SkillLine
-        name  = 'SQL'
-        width = '30%'
-    />  
-    <SkillLine
-        name  = 'MongoDB \ Mongoose'
-        width = '30%'
-    />  
-    <SkillLine
-        name  = 'Authentication & Security'
-        width = '30%'
-    />  
-    <SkillLine
-        name  = 'Wordpress'
-        width = '30%'
-    />  
-    <SkillLine
-        name  = 'JQuery'
-        width = '50%'
-    />  
-    <SkillLine
-        name  = 'SVG'
-        width = '70%'
-    />  
-    <SkillLine
-        name  = 'PHP'
-        width = '10%'
-    />  
-    <SkillLine
-        name  = 'WEB 3 (Motoko, Decentrelized Apps (DApps), Internet Computer, Blockchain)'
-        width = '10%'
-    />  
+    {attributes_SkillLine.map(create_SkillLine)}
     <h2 className = "section-title">
         <span className = "red"> ▶ [ </span>
         Soft Skills
@@ -118,3 +152,5 @@ export default () =>
     </div>
 </section>
 )
+
+export default SectionB
--- src/SectionD.jsx
+++ src/SectionD.jsx
@@ -3,29 +3,108 @@
 import React from 'react'
 import WebSitePreview from './WebSitePreview'
 
-// Lazy Loading Images
-// const imgTargets = document.querySelectorAll('img[data-src]')
-// const loadImg = (entries, observer) =>
-// {
-//     const [entry] = entries;
+const attributes_WebSitePreview =
+[
+    {
+        id          : 1,
+        dataSrc     : 'img/items/prew-a10-lazy.jpg',
+        src         : 'img/items/prew-a10.jpg',
+        href        : 'https://elegant-spence-1f45ba.netlify.app/',
+        WebSiteName : 'A-10 LAB'  
+    },
+    {
+        id          : 2,
+        dataSrc     : 'img/items/prew-bukee-lazy.jpg',
+        src         : 'img/items/prew-bukee.jpg',
+        href        : 'https://cranky-wescoff-fb4651.netlify.com',
+        WebSiteName : 'BUKEE'
+    },
+    {
+        id          : 3,
+        dataSrc     : 'img/items/prew-B152-black-lazy.jpg',
+        src         : 'img/items/prew-B152-black.jpg',
+        href        : 'https://cocky-spence-cedba8.netlify.app/',
+        WebSiteName : 'B152_Black'
+    },
+    {
+        id          : 4,
+        dataSrc     : 'img/items/prew-bankist-lazy.jpg',
+        src         : 'img/items/prew-bankist.jpg',
+        href        : 'https://vibrant-stonebraker-718e7f.netlify.app/',
+        WebSiteName : 'BANKIST'
+    },
+    {
+        id          : 5,
+        dataSrc     : 'img/items/prew-edgeledger-lazy.jpg',
+        src         : 'img/items/prew-edgeledger.jpg',
+        href        : 'https://ecstatic-poincare-15b597.netlify.com',
+        WebSiteName : 'EdgeLedger'
+    },
+    {
+        id          : 6,
+        dataSrc     : 'img/items/prew-grid-lazy.jpg',
+        src         : 'img/items/prew-grid.jpg',
+        href        : 'https://competent-banach-9fdd6d.netlify.com',
+        WebSiteName : 'GridNews'
+    },
+    {
+        id          : 7,
+        dataSrc     : 'img/items/prew-hw-b-lazy.jpg',
+        src         : 'img/items/prew-hw-b.jpg',
+        href        : 'https://practical-curie-6a7c54.netlify.com',
+        WebSiteName : 'HW-B'
+    },
+    {
+        id          : 8,
+        dataSrc     : 'img/items/prew-presentation-lazy.jpg',
+        src         : 'img/items/prew-presentation.jpg',
+        href        : 'https://zen-hopper-2ebf1f.netlify.com/',
+        WebSiteName : 'Presentation'
+    },
+    {
+        id          : 9,
+        dataSrc     : 'img/items/prew-timeline-lazy.jpg',
+        src         : 'img/items/prew-timeline.jpg',
+        href        : 'https://competent-bhaskara-328919.netlify.com',
+        WebSiteName : 'Base 4 Timeline<'
+    },
+    {
+        id          : 10,
+        dataSrc     : 'img/items/prew-KittyLab-lazy.jpg', 
+        src         : 'img/items/prew-KittyLab.jpg',
+        href        : 'https://confident-banach-cc4a1e.netlify.com/',
+        WebSiteName : 'KittyLab'
+    },
+    {
+        id          : 11,
+        dataSrc     : 'img/items/prew-B152-lazy.jpg', 
+        src         : 'img/items/prew-B152.jpg',
+        href        : 'https://confident-mahavira-1727f1.netlify.app/',
+        WebSiteName : 'B152'
+    },
+    {
+        id          : 12,
+        dataSrc     : 'img/items/prew-Re-lazy.jpg',
+        src         : 'img/items/prew-Re.jpg',
+        href        : 'https://laughing-poincare-a5b323.netlify.app/',
+        WebSiteName : 'Re:'
+    }
+]
 
-//     if (!entry.isIntersecting) return;
+const create_WebSitePreview = (attribute) =>
+{
+    return(
+        <WebSitePreview
+            dataSrc     = {attribute.dataSrc}
+            src         = {attribute.src}
+            href        = {attribute.href}
+            WebSiteName = {attribute.WebSiteName}
+            key         = {attribute.id}
+        />
+    )
+}
 
-//     // Replace src with data-src
-//     entry.target.src = entry.target.dataset.src;
-//     entry.target.addEventListener('load', () => entry.target.classList.remove('lazy-img'));
-//     observer.unobserve(entry.target);
-// };
-
-// const imgObserver = new IntersectionObserver(loadImg, 
-// {
-// root: null,
-// threshold: 0,
-// rootMargin: '200px',
-// });
-// imgTargets.forEach(img => imgObserver.observe(img));
-
-export default () =>
+const SectionB = () =>
 (
 <section id="work-a" className = "text-center py-1">
 <div className = "container">
@@ -40,80 +119,10 @@ export default () =>
         </p>
     </h2>
     <div className = "items py-4">
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-a10-lazy.jpg"
-            src         = "img/items/prew-a10.jpg"
-            href        = "https://elegant-spence-1f45ba.netlify.app/"
-            WebSiteName = "A-10 LAB"   
-        />
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-bukee-lazy.jpg"
-            src         = "img/items/prew-bukee.jpg"
-            href        = "https://cranky-wescoff-fb4651.netlify.com"
-            WebSiteName = "BUKEE"
-        />
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-B152-black-lazy.jpg"
-            src         = "img/items/prew-B152-black.jpg"
-            href        = "https://cocky-spence-cedba8.netlify.app/"
-            WebSiteName = "B152_Black"
-        />
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-bankist-lazy.jpg"
-            src         = "img/items/prew-bankist.jpg"
-            href        = "https://vibrant-stonebraker-718e7f.netlify.app/"
-            WebSiteName = "BANKIST"
-        />
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-edgeledger-lazy.jpg"
-            src         = "img/items/prew-edgeledger.jpg"
-            href        = "https://ecstatic-poincare-15b597.netlify.com"
-            WebSiteName = "EdgeLedger"
-        />
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-grid-lazy.jpg"
-            src         = "img/items/prew-grid.jpg"
-            href        = "https://competent-banach-9fdd6d.netlify.com"
-            WebSiteName = "GridNews"
-        />
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-hw-b-lazy.jpg" 
-            src         = "img/items/prew-hw-b.jpg"
-            href        = "https://practical-curie-6a7c54.netlify.com" 
-            WebSiteName = "HW-B"
-        />
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-presentation-lazy.jpg" 
-            src         = "img/items/prew-presentation.jpg"
-            href        = "https://zen-hopper-2ebf1f.netlify.com/"
-            WebSiteName = "Presentation"
-        />
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-timeline-lazy.jpg"
-            src         = "img/items/prew-timeline.jpg"
-            href        = "https://competent-bhaskara-328919.netlify.com"
-            WebSiteName = "Timeline"
-        />
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-KittyLab-lazy.jpg" 
-            src         = "img/items/prew-KittyLab.jpg"
-            href        = "https://confident-banach-cc4a1e.netlify.com/"
-            WebSiteName = "KittyLab"
-        />
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-B152-lazy.jpg" 
-            src         = "img/items/prew-B152.jpg"
-            href        = "https://confident-mahavira-1727f1.netlify.app/"
-            WebSiteName = "B152"
-        />
-        <WebSitePreview 
-            dataSrc     = "img/items/prew-Re-lazy.jpg"
-            src         = "img/items/prew-Re.jpg"
-            href        = "https://laughing-poincare-a5b323.netlify.app/"
-            WebSiteName = "Re:"
-        />
+        {attributes_WebSitePreview.map(create_WebSitePreview)}
     </div>
 </div>
 </section>
 )
 
+export default SectionB
